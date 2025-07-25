0a882ee6162b6e94cf0985f218e5a32cd18cba4c
Amit-Mahato-0001 <amitmahatocky909@gmail.com>
2025-07-25 23:28:46 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// In vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/', // For root domain deployment
+  plugins: [react()],
+})
